504ac714bee24caa8cf617c25de410eda6cc76a2
Vadim <31937223+VadimOreshkevich@users.noreply.github.com>
2017-10-19 00:15:45 +0300

    Загрузил файлы и папки
    Добавил файлы сайта

---
+++ js/popup.js
@@ -0,0 +1,9 @@
+var popupDiv = document.getElementsByClassName('popup-bg');
+
+function openPopupMenu() {
+		popupDiv.style.display = "block";
+}
+
+function closePopupMenu() {
+		popupDiv.style.display = "none";
+}
